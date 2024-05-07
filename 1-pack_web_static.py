f4500dc64eab036a7b89a62ac5844e63016baa81
Esumei Moses <archer.me24@gmail.com>
2024-05-07 07:02:42 -0700

    generate a .tgz archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,22 +2,25 @@
 from fabric.api import run, env, local, task
 import os
 from datetime import datetime
+
+
 """using fabric generate a .tgz archive of the web_static folder"""
 
 
 @task
 def do_pack():
-    """fab function"""
-    #hosts = ['ubuntu@100.25.143.79', 'ubuntu@100.25.222.39']
-    #for env.host_string in hosts:
-    #    print(env.host_string)
-    #    run("whoami")
-    #    run('ls -l')
+    """fab function
+    hosts = ['ubuntu@100.25.143.79', 'ubuntu@100.25.222.39']
+    for env.host_string in hosts:
+        print(env.host_string)
+        run("whoami")
+        run('ls -l')
+    """
     try:
         source_dir = 'web_static'
         destination_dir = 'versions'
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        
+
         # Archive file name
         arch_filename = f'web_static_{timestamp}.tgz'
 
@@ -33,5 +36,5 @@ def do_pack():
 
         return None
 
+
 do_pack()
-    
