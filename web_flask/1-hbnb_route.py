f62f0513e55aa561185e02133db9ad982b1df2ce
Esumei Moses <archer.me24@gmail.com>
2024-05-19 09:14:01 -0700

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,10 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route("/hbnb", strict_slashes=False)
 def Hello_hbnb():
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
