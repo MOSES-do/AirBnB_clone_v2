f8df2d029b567c5ad832305ef9120a62f47799d9
Esumei Moses <archer.me24@gmail.com>
2024-05-18 17:35:40 -0700

    Python for the web - Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+"""Create instanc of WSGI app from flask class"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+	"""flask mini app"""
+	return f"Hello HBNB!"
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
