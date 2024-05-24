c49a4b5f090d5ee38a864cbbfc21cd62d913d144
Esumei Moses <archer.me24@gmail.com>
2024-05-24 12:49:50 -0700

    flask web app

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -5,14 +5,14 @@ Flask Web application
 
 from flask import Flask, render_template
 from models.state import State
-from models.amenities import Amenity
+from models.amenity import Amenity
 from models import storage
 app = Flask(__name__)
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def filters():
-    """display a HTML page like 6-index.html from static"""
+    """display an HTML page from static"""
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
     return render_template('10-hbnb_filters.html', states=states,
@@ -25,4 +25,4 @@ def teardown_db(exception):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
